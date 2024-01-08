b61240fd1a739e58063208fb64554cbda46f8aaf
albadelriodiaz <adelr22@fpsanjorge.com>
2024-01-08 14:21:17 +0100

    laravel proiektuaren hasiera: crear, editar y eliminar hecho

---
+++ laravel_e2t3/app/Http/Controllers/taldea_controller.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Taldea;
+
+class taldea_controller extends Controller
+{
+    public function index(){
+        $table = Taldea::all();
+        return response()->json($table, 200);
+    }
+
+    public function erakutsi($kodea){
+        $table = Taldea::where('kodea', $kodea)->first();
+        return response()->json($table, 200);
+    }
+
+    //probamos así http://localhost/Laravel/6_ariketa/public/api/taldearuta/1
+
+    public function gorde(Request $aux)
+    {
+        // Valida los datos del formulario según tus necesidades
+        $this->validate($aux, [
+            'kodea' => 'required',
+            'izena' => 'required',
+        ]);
+
+        // Eloquent erabiliz modeloa sortu
+        //$nuevoBeharra = new Taldea;
+        $nuevoTalde = array(
+            'kodea' => $aux->input('kodea'),
+            'izena' => $aux->input('izena'),
+            'created_at' => now()
+        );
+
+        // Guarda el nuevo registro en la base de datos
+        Taldea::insert($nuevoTalde);
+
+        // Datu-basean ondo gorde den erantzuna 201 status kodearekin itzuliko da
+        return response()->json($nuevoTalde, 201);
+    }
+
+
+
+
+
+
+    public function eguneratu(Request $aux, $kodea)
+    {
+        // Valida los datos del formulario según tus necesidades
+        $this->validate($aux, [
+            'kodea' => 'required',
+            'izena' => 'required',
+        ]);
+
+        $taldeEguneratuta = Taldea::where('kodea', $kodea)->first();
+
+        // Si no se encuentra el registro, devuelve un error 404
+        if (!$taldeEguneratuta) {
+            return response()->json(['error' => 'Registro no encontrado'], 404);
+        }
+
+        // Actualiza los valores del modelo con los datos del formulario
+        $taldeEguneratuta->update([
+            'kodea' => $aux->input('kodea'),
+            'izena' => $aux->input('izena'),
+            'updated_at' => now()
+        ]);
+
+        // Devuelve el registro actualizado con un código de estado 200
+        return response()->json($taldeEguneratuta, 200);
+    }
+
+
+
+
+
+    public function ezabatu(Request $aux, $kodea)
+    {
+        // Valida los datos del formulario según tus necesidades
+        $this->validate($aux, [
+            'kodea' => 'required'
+        ]);
+
+        $taldeEguneratuta = Taldea::where('kodea', $kodea)->first();
+
+        // Si no se encuentra el registro, devuelve un error 404
+        if (!$taldeEguneratuta) {
+            return response()->json(['error' => 'Registro no encontrado'], 404);
+        }
+
+        // Actualiza los valores del modelo con los datos del formulario
+        $taldeEguneratuta->update([
+            'deleted_at' => now()
+        ]);
+
+        // Devuelve el registro actualizado con un código de estado 200
+        return response()->json($taldeEguneratuta, 200);
+    }
+}
---
+++ laravel_e2t3/app/Models/Taldea.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Taldea extends Model
+{
+    
+    protected $table = 'taldea';
+
+    protected $primaryKey = 'kodea';
+
+    protected $fillable = [
+        'izena', 'updated_at', 'deleted_at', 
+    ];
+
+    protected $guarded = [
+        'kodea',
+    ];
+}
