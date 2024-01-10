7826f6d1d431aa3dab132643e506b3b3158d0efd
albadelriodiaz <adelr22@fpsanjorge.com>
2024-01-10 12:22:16 +0100

    gitignore comentado para subir todo

--- laravel_e2t3/app/Http/Controllers/ordutegia_controller.php
+++ laravel_e2t3/app/Http/Controllers/ordutegia_controller.php
@@ -20,10 +20,9 @@ class ordutegia_controller extends Controller
             'eguna' => $aux->input('eguna'),
             'hasiera_data' => $aux->input('hasiera_data'),
             'amaiera_data' => $aux->input('amaiera_data'),
-            'hasiera_ordua' => $aux->input('hasiera_data'),
-            'amaiera_ordua' => $aux->input('hasiera_data'),
+            'hasiera_ordua' => $aux->input('hasiera_ordua'),
+            'amaiera_ordua' => $aux->input('amaiera_ordua'),
             'created_at' => now()
-
         );
 
         // Guarda el nuevo registro en la base de datos
@@ -31,6 +30,30 @@ class ordutegia_controller extends Controller
 
         // Datu-basean ondo gorde den erantzuna 201 status idrekin itzuliko da
         return response()->json($nuevoHorario, 201);
+    }
+
+    public function eguneratu(Request $aux, $id)
+    {
+        $ordutegiEguneratuta = Ordutegia::where('id', $id)->first();
+        
+        // Si no se encuentra el registro, devuelve un error 404
+        if (!$ordutegiEguneratuta) {
+            return response()->json(['error' => 'Registro no encontrado'], 404);
+        }
+        
+
+        // Actualiza los valores del modelo con los datos del formulario
+        $ordutegiEguneratuta->update([
+            'eguna' => $aux->input('eguna'),
+            'hasiera_data' => $aux->input('hasiera_data'),
+            'amaiera_data' => $aux->input('amaiera_data'),
+            'hasiera_ordua' => $aux->input('hasiera_ordua'),
+            'amaiera_ordua' => $aux->input('amaiera_ordua'),
+            'updated_at' => now()
+        ]);
+
+        // Devuelve el registro actualizado con un código de estado 200
+        return response()->json($ordutegiEguneratuta, 200);
     }
 
 }
