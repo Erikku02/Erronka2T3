047d448aa5e63465f7cc0467b61f887bc6e5d574
Erikku02 <egarcial22@fpsanjorge.com>
2024-02-08 12:13:31 +0100

    sildñkhgsaklngvñlsenf

--- laravel_e2t3/routes/api.php
+++ laravel_e2t3/routes/api.php
@@ -45,15 +45,15 @@ Route::get('taldearuta/{kodea}', [taldea_controller::class, 'erakutsi']);
 
 // Route::post('taldeagorde', 'App\Http\Controllers\taldeac_controller@gorde');
 
-Route::get('taldeagorde', [taldea_controller::class, 'gorde']);
+Route::post('taldeagorde', [taldea_controller::class, 'gorde']);
 
 // Route::put('taldeaeguneratu/{kodea}', 'App\Http\Controllers\taldeac_controller@eguneratu');
 
-Route::get('taldeaeguneratu/{kodea}', [taldea_controller::class, 'eguneratu']);
+Route::put('taldeaeguneratu/{kodea}', [taldea_controller::class, 'eguneratu']);
 
 // Route::put('taldeaezabatu/{kodea}', 'App\Http\Controllers\taldeac_controller@ezabatu');
 
-Route::get('taldeaezabatu/{kodea}', [taldea_controller::class, 'ezabatu']);
+Route::put('taldeaezabatu/{kodea}', [taldea_controller::class, 'ezabatu']);
 
 //Langileak
 
@@ -71,15 +71,15 @@ Route::get('langilearutaTxanda', [langilea_controller::class, 'mostrarTablaTraba
 
 // Route::post('langileagorde', 'App\http\Controllers\langilea_controller@gorde');
 
-Route::get('langileagorde', [langilea_controller::class, 'gorde']);
+Route::post('langileagorde', [langilea_controller::class, 'gorde']);
 
 // Route::put('langileaeguneratu/{id}', 'App\Http\Controllers\langilea_controller@eguneratu');
 
-Route::get('langileaeguneratu/{id}', [langilea_controller::class, 'eguneratu']);
+Route::put('langileaeguneratu/{id}', [langilea_controller::class, 'eguneratu']);
 
 // Route::put('langileaezabatu/{id}', 'App\Http\Controllers\langilea_controller@ezabatu');
 
-Route::get('langileaezabatu/{id}', [langilea_controller::class, 'ezabatu']);
+Route::put('langileaezabatu/{id}', [langilea_controller::class, 'ezabatu']);
 
 //otra forma de hacerlo:
 //Route::get('taldearuta', [taldea_controller::class, 'index']);
@@ -92,15 +92,15 @@ Route::get('ordutegiaruta', [ordutegia_controller::class, 'index']);
 
 // Route::post('ordutegiagorde', 'App\http\Controllers\ordutegia_controller@gorde');
 
-Route::get('ordutegiagorde', [ordutegia_controller::class, 'gorde']);
+Route::post('ordutegiagorde', [ordutegia_controller::class, 'gorde']);
 
 // Route::put('ordutegiaeguneratu/{id}', 'App\Http\Controllers\ordutegia_controller@eguneratu');
 
-Route::get('ordutegiaeguneratu/{id}', [ordutegia_controller::class, 'eguneratu']);
+Route::put('ordutegiaeguneratu/{id}', [ordutegia_controller::class, 'eguneratu']);
 
 // Route::put('ordutegiaezabatu/{id}', 'App\Http\Controllers\ordutegia_controller@ezabatu');
 
-Route::get('ordutegiaezabatu/{id}', [ordutegia_controller::class, 'ezabatu']);
+Route::put('ordutegiaezabatu/{id}', [ordutegia_controller::class, 'ezabatu']);
 
 //Kategoriak
 
@@ -110,15 +110,15 @@ Route::get('kategoriaruta', [kategoria_controller::class, 'index']);
 
 // Route::post('kategoriagorde', 'App\http\Controllers\kategoria_controller@gorde');
 
-Route::get('kategoriagorde', [kategoria_controller::class, 'gorde']);
+Route::post('kategoriagorde', [kategoria_controller::class, 'gorde']);
 
 // Route::put('kategoriaeguneratu/{id}', 'App\Http\Controllers\kategoria_controller@eguneratu');
 
-Route::get('kategoriaeguneratu/{id}', [kategoria_controller::class, 'eguneratu']);
+Route::put('kategoriaeguneratu/{id}', [kategoria_controller::class, 'eguneratu']);
 
 // Route::put('kategoriaezabatu/{id}', 'App\Http\Controllers\kategoria_controller@ezabatu');
 
-Route::get('kategoriaezabatu/{id}', [kategoria_controller::class, 'ezabatu']);
+Route::put('kategoriaezabatu/{id}', [kategoria_controller::class, 'ezabatu']);
 
 //así habría que probarlo en la URL, ejemplo:
 //http://localhost/Laravel/6_ariketa/public/api/taldearuta
@@ -135,7 +135,7 @@ Route::get('txandaruta/{kodea}', [txanda_controller::class, 'erakutsi']);
 
 // Route::post('txandagorde', 'App\http\Controllers\txanda_controller@gorde');
 
-Route::get('txandagorde', [txanda_controller::class, 'gorde']);
+Route::post('txandagorde', [txanda_controller::class, 'gorde']);
 
 // Route::get('txandaMostradorea/{kodea}', 'App\http\Controllers\txanda_controller@azkenMostradorea');
 
@@ -157,15 +157,15 @@ Route::get('materialaruta', [materiala_controller::class, 'index']);
 
 // Route::post('materialagorde', 'App\http\Controllers\materiala_controller@gorde');
 
-Route::get('materialagorde', [materiala_controller::class, 'gorde']);
+Route::post('materialagorde', [materiala_controller::class, 'gorde']);
 
 // Route::put('materialaeguneratu/{id}', 'App\Http\Controllers\materiala_controller@eguneratu');
 
-Route::get('materialaeguneratu/{id}', [materiala_controller::class, 'eguneratu']);
+Route::put('materialaeguneratu/{id}', [materiala_controller::class, 'eguneratu']);
 
 // Route::put('materialaezabatu/{id}', 'App\Http\Controllers\materiala_controller@ezabatu');
 
-Route::get('materialaezabatu/{id}', [materiala_controller::class, 'ezabatu']);
+Route::put('materialaezabatu/{id}', [materiala_controller::class, 'ezabatu']);
 
 // Materiala_erabili
 
@@ -175,15 +175,15 @@ Route::get('materialaerabiliruta', [materiala_erabili_controller::class, 'index'
 
 // Route::post('materialaerabiligorde', 'App\http\Controllers\materiala_erabili_controller@gorde');
 
-Route::get('materialaerabiligorde', [materiala_erabili_controller::class, 'gorde']);
+Route::post('materialaerabiligorde', [materiala_erabili_controller::class, 'gorde']);
 
 // Route::put('materialaerabilieguneratu/{id}', 'App\Http\Controllers\materiala_erabili_controller@eguneratu');
 
-Route::get('materialaerabilieguneratu/{id}', [materiala_erabili_controller::class, 'eguneratu']);
+Route::put('materialaerabilieguneratu/{id}', [materiala_erabili_controller::class, 'eguneratu']);
 
 // Route::put('materialaerabiliezabatu/{id}', 'App\Http\Controllers\materiala_erabili_controller@ezabatu');
 
-Route::get('materialaerabiliezabatu/{id}', [materiala_erabili_controller::class, 'ezabatu']);
+Route::put('materialaerabiliezabatu/{id}', [materiala_erabili_controller::class, 'ezabatu']);
 
 //Produktuak
 
@@ -197,15 +197,15 @@ Route::get('markaruta', [produktua_controller::class, 'ateraMarka']);
 
 // Route::post('produktuagorde', 'App\http\Controllers\produktua_controller@gorde');
 
-Route::get('produktuagorde', [produktua_controller::class, 'gorde']);
+Route::post('produktuagorde', [produktua_controller::class, 'gorde']);
 
 // Route::put('produktuaeguneratu/{id}', 'App\Http\Controllers\produktua_controller@eguneratu');
 
-Route::get('produktuaeguneratu/{id}', [produktua_controller::class, 'eguneratu']);
+Route::put('produktuaeguneratu/{id}', [produktua_controller::class, 'eguneratu']);
 
 // Route::put('produktuaezabatu/{id}', 'App\Http\Controllers\produktua_controller@ezabatu');
 
-Route::get('produktuaezabatu/{id}', [produktua_controller::class, 'ezabatu']);
+Route::put('produktuaezabatu/{id}', [produktua_controller::class, 'ezabatu']);
 
 
 //Produktu-Mugimenduak
@@ -218,7 +218,7 @@ Route::get('produktumugimenduaruta', [produktu_mugimendua_controller::class, 'in
 
 // Route::post('produktumugimenduagorde', 'App\http\Controllers\produktu_mugimendua_controller@gorde');
 
-Route::get('produktumugimenduagorde', [produktu_mugimendua_controller::class, 'gorde']);
+Route::post('produktumugimenduagorde', [produktu_mugimendua_controller::class, 'gorde']);
 
 // Route::put('produktuaeguneratu/{id}', 'App\Http\Controllers\produktua_controller@eguneratu');
 
@@ -233,15 +233,15 @@ Route::get('tratamenduaruta', [tratamendua_controller::class, 'index']);
 
 // Route::post('tratamenduagorde', 'App\http\Controllers\tratamendua_controller@gorde');
 
-Route::get('tratamenduagorde', [tratamendua_controller::class, 'gorde']);
+Route::post('tratamenduagorde', [tratamendua_controller::class, 'gorde']);
 
 // Route::put('tratamenduaeguneratu/{id}', 'App\Http\Controllers\tratamendua_controller@eguneratu');
 
-Route::get('tratamenduaeguneratu/{id}', [tratamendua_controller::class, 'eguneratu']);
+Route::put('tratamenduaeguneratu/{id}', [tratamendua_controller::class, 'eguneratu']);
 
 // Route::put('tratamenduaezabatu/{id}', 'App\Http\Controllers\tratamendua_controller@ezabatu');
 
-Route::get('tratamenduaezabatu/{id}', [tratamendua_controller::class, 'ezabatu']);
+Route::put('tratamenduaezabatu/{id}', [tratamendua_controller::class, 'ezabatu']);
 
 
 //Bezero_fitxak
@@ -252,15 +252,15 @@ Route::get('bezero_fixaruta', [bezero_fitxa_controller::class, 'index']);
 
 // Route::post('bezero_fixagorde', 'App\http\Controllers\bezero_fitxa_controller@gorde');
 
-Route::get('bezero_fixagorde', [bezero_fitxa_controller::class, 'gorde']);
+Route::post('bezero_fixagorde', [bezero_fitxa_controller::class, 'gorde']);
 
 // Route::put('bezero_fixaeguneratu/{id}', 'App\Http\Controllers\bezero_fitxa_controller@eguneratu');
 
-Route::get('bezero_fixaeguneratu/{id}', [bezero_fitxa_controller::class, 'eguneratu']);
+Route::put('bezero_fixaeguneratu/{id}', [bezero_fitxa_controller::class, 'eguneratu']);
 
 // Route::put('bezero_fixaezabatu/{id}', 'App\Http\Controllers\bezero_fitxa_controller@ezabatu');
 
-Route::get('bezero_fixaezabatu/{id}', [bezero_fitxa_controller::class, 'ezabatu']);
+Route::put('bezero_fixaezabatu/{id}', [bezero_fitxa_controller::class, 'ezabatu']);
 
 
 //Kolore_historiala
@@ -271,15 +271,15 @@ Route::get('kolore_histruta', [kolore_historiala_controller::class, 'index']);
 
 // Route::post('kolore_histgorde', 'App\http\Controllers\kolore_historiala_controller@gorde');
 
-Route::get('kolore_histgorde', [kolore_historiala_controller::class, 'gorde']);
+Route::post('kolore_histgorde', [kolore_historiala_controller::class, 'gorde']);
 
 // Route::put('kolore_histeguneratu/{id}', 'App\Http\Controllers\kolore_historiala_controller@eguneratu');
 
-Route::get('kolore_histeguneratu/{id}', [kolore_historiala_controller::class, 'eguneratu']);
+Route::put('kolore_histeguneratu/{id}', [kolore_historiala_controller::class, 'eguneratu']);
 
 // Route::put('kolore_histezabatu/{id}', 'App\Http\Controllers\kolore_historiala_controller@ezabatu');
 
-Route::get('kolore_histezabatu/{id}', [kolore_historiala_controller::class, 'ezabatu']);
+Route::put('kolore_histezabatu/{id}', [kolore_historiala_controller::class, 'ezabatu']);
 
 
 //así habría que probarlo en la URL, ejemplo:
@@ -294,15 +294,15 @@ Route::get('hitzorduaruta', [hitzordua_controller::class, 'index']);
 
 // Route::post('hitzorduagorde', 'App\http\Controllers\hitzordua_controller@gorde');
 
-Route::get('hitzorduagorde', [hitzordua_controller::class, 'gorde']);
+Route::post('hitzorduagorde', [hitzordua_controller::class, 'gorde']);
 
 // Route::put('hitzorduaeguneratu/{id}', 'App\Http\Controllers\hitzordua_controller@eguneratu');
 
-Route::get('hitzorduaeguneratu/{id}', [hitzordua_controller::class, 'eguneratu']);
+Route::put('hitzorduaeguneratu/{id}', [hitzordua_controller::class, 'eguneratu']);
 
 // Route::put('hitzorduaezabatu/{id}', 'App\Http\Controllers\hitzordua_controller@ezabatu');
 
-Route::get('hitzorduaezabatu/{id}', [hitzordua_controller::class, 'ezabatu']);
+Route::put('hitzorduaezabatu/{id}', [hitzordua_controller::class, 'ezabatu']);
 
 
 //Materiala erabili
@@ -313,8 +313,8 @@ Route::get('materialaerabiliruta', [materiala_erabili_controller::class, 'index'
 
 // Route::post('materialaerabiligorde', 'App\http\Controllers\materiala_erabili_controller@gorde');
 
-Route::get('materialaerabiligorde', [materiala_erabili_controller::class, 'gorde']);
+Route::post('materialaerabiligorde', [materiala_erabili_controller::class, 'gorde']);
 
 // Route::put('materialaerabiliezabatu/{id}', 'App\Http\Controllers\materiala_erabili_controller@ezabatu');
 
-Route::get('materialaerabiliezabatu/{id}', [materiala_erabili_controller::class, 'ezabatu']);
+Route::put('materialaerabiliezabatu/{id}', [materiala_erabili_controller::class, 'ezabatu']);
